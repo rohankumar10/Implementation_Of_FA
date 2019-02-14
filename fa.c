a4c7c4010fae5c7e64691ca0622b31a47e3edaa8
rohankumar10 <36202705+rohankumar10@users.noreply.github.com>
2019-02-14 21:24:21 +0530

    Update fa.c

--- fa.c
+++ fa.c
@@ -12,24 +12,23 @@ int InitialState,FinalState,CurrentState;
 InitialState=0;
 FinalState=2;
 CurrentState=InitialState;
-char w[100]="10001010"; // Input String
+char S[100]="10001010"; // Input String
 i=0; // Counter
 	
 	while(i<strlen(w))
 	{	
-	printf("%d",CurrentState);
-	CurrentState=TT[CurrentState][(w[i]-'0')];
+	CurrentState=TT[CurrentState][(S[i]-'0')];
 	printf("CurrentState: %d, SymbolToPrint: %d, Transactions: %d\n",
-	CurrentState, w[i]-'0',i+1);	
+	CurrentState, S[i]-'0',i+1);	
 	i++;
 	}
 		if(CurrentState==FinalState)
 		{
-		printf("%s Accepted",w);
+		printf("%s Accepted",S);
 		}
 			else
 			{
-			printf("%s is Rejected",w);
+			printf("%s is Rejected",S);
 			}
 return 0;
 }
